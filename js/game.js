1ce8ea53c90ea114045c8934947a0ee334dc116c
cBk91 <wojciech.pielak@wir.pl>
2024-08-08 23:56:32 +0200

    Game logic
    Create playRound function and call it 5 times to simulate game.

--- js/game.js
+++ js/game.js
@@ -5,10 +5,50 @@ const PAPER = "paper";
 let humanScore = 0;
 let computerScore = 0;
 
+for(let i = 0,humanChoice = 0; i < 5;i++){
+    do{
+        humanChoice = prompt("Type your choice: ");
+    }while(!isHumanChoiceCorrect(humanChoice));
+    console.log(playRound(humanChoice,getComputerChoice()));
+}
 
+function playRound(humanChoice,computerChoice){
+    console.log(`Human: ${humanChoice} PC: ${computerChoice}`);
+    if(humanChoice === computerChoice)        
+        return "It is a draw!";    
+    let result = 0;
+    
+    switch(humanChoice){
+        case "rock":
+            result = decideWhoLose(computerChoice,SCISSORS);
+            break;
+
+        case "paper":
+            result = decideWhoLose(computerChoice,ROCK);
+            break;
+
+        case "scissors":
+            result = decideWhoLose(computerChoice,PAPER);
+            break;
+    }
+    return result === 1 ?`You win! ${humanChoice} beats ${computerChoice}.`:
+    `You lose! ${computerChoice} beats ${humanChoice}.`;        
+}
+
+function decideWhoLose(computerChoice,loseChoice){
+    if(computerChoice === loseChoice){
+        humanScore++;                
+        return 1;
+        }
+    else{        
+        computerScore++;
+        return 2;    
+    }
+}
 
 function isHumanChoiceCorrect(choice){
-    return choice === "rock" || choice === "paper" || choice ==="scissors";
+    return choice.toLowerCase() === "rock" || 
+    choice.toLowerCase() === "paper" || choice.toLowerCase() ==="scissors";
 }
 
 
@@ -25,15 +65,16 @@ function getComputerChoice(){
 }
 
 function getHumanChoice(choice){
-    return choiceSignal(convertStringChoiceInNumber(choice.toLowerCase()));
+    return choiceSignal(convertStringChoiceToNumber(choice.toLowerCase()));
 }
-function convertStringChoiceInNumber(choice){
+function convertStringChoiceToNumber(choice){
     switch(choice){
         case "rock":return 0;
         case "paper":return 1;
         case "scissors":return 2;
     }
 }
+
 function getRandomInt(max){
     return Math.floor(Math.random() * max) ;
 }
